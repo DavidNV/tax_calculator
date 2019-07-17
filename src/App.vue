e66a7c08ae46fda6cab36110fa5f068419577bf2
david.valderrama <david.valderrama@linkbal.co.jp>
2019-07-17 10:34:27 +0900

    add sass

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Panel,
+    InputForm,
+    Result
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
